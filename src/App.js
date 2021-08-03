9037b06e374cdb1177844a990594cc0310597f90
Doanh2406 <lxqdoanh.17it1@vku.udn.vn>
2021-08-03 15:48:48 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="clas">
+        hello ecomm
+      </h1>
+      <button className="th">click me</button>
+      <Button className="btn btn-primary">click me</Button>
+    </div>
+  );
+}
+
+export default App;
