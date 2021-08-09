e748188502c0f73f6a941627bf399f457ed08917
t3n1o12 <lvnhat.17it1@vku.udn.vn>
2021-08-09 15:25:00 -0700

    asa

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -7,29 +7,89 @@ import TimelineOutlinedIcon from '@material-ui/icons/TimelineOutlined';
 import Chart from './Chart'
 import Chart2 from './Chart2'
 import './Payment.css'
-import {Table} from '../../elements'
+import { Table } from '../../elements'
+import { Bar, Pie } from 'react-chartjs-2';
 
+const datas = {
+  labels: ['Total', 'Tax',],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [10,90],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+const data = {
+  labels: ['1', '2', '3', '4', '5', '6'],
+  datasets: [
+    {
+      label: 'Total $',
+      data: [12, 19, 20, 5, 2, 3],
+      backgroundColor: 'rgb(255, 99, 132)',
+    },
+    {
+      label: 'Earning',
+      data: [2, 3, 17, 3, 1, 2],
+      backgroundColor: 'rgb(54, 162, 235)',
+    },
+   
+  ],
+};
 
+const options = {
+  scales: {
+    yAxes: [
+      {
+        stacked: true,
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+    xAxes: [
+      {
+        stacked: true,
+      },
+    ],
+  },
+};
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width:'87%',
-    marginLeft:10,
+    width: '87%',
+    marginLeft: 10,
     marginTop: 20,
-    
+
   },
   paper: {
     padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
-   
+
   },
- 
+
 }));
 
 export default function Payment() {
   const classes = useStyles();
- 
+
 
   return (
     <div className={classes.root}>
@@ -50,7 +110,7 @@ export default function Payment() {
               <TimelineOutlinedIcon style={{ marginLeft: 'auto', color: '#fff', fontSize: 40 }} />
             </div>
             <div style={{ width: '100%', alignItems: 'center', justifyItems: 'center' }}>
-              <Chart />
+            <Pie data={datas} />
             </div>
           </Paper>
         </Grid>
@@ -69,7 +129,7 @@ export default function Payment() {
               <TimelineOutlinedIcon style={{ marginLeft: 'auto', color: '#fff', fontSize: 40 }} />
             </div>
             <div style={{ width: '100%', alignItems: 'center', justifyItems: 'center' }}>
-              <Chart />
+            <Pie data={datas} />
             </div>
           </Paper>
         </Grid>
@@ -88,7 +148,7 @@ export default function Payment() {
               <TimelineOutlinedIcon style={{ marginLeft: 'auto', color: '#fff', fontSize: 40 }} />
             </div>
             <div style={{ width: '100%', alignItems: 'center', justifyItems: 'center' }}>
-              <Chart />
+            <Pie data={datas} />
             </div>
           </Paper>
         </Grid>
@@ -107,23 +167,34 @@ export default function Payment() {
               <TimelineOutlinedIcon style={{ marginLeft: 'auto', color: '#fff', fontSize: 40 }} />
             </div>
             <div style={{ width: '100%', alignItems: 'center', justifyItems: 'center' }}>
-              <Chart />
+            <Pie data={datas} />
             </div>
           </Paper>
         </Grid>
       </Grid>
-      <Grid xs={12}>
-        <Chart2 />
+      <div style={{marginTop:20}}/>
+      <Grid container xs={12} spacing={2} >
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+          <Typography variant="h4" >
+                  Earning in week
+                </Typography>
+                  <div style={{height:10}}/>
+            <Bar data={data} options={options} />
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+            <p style={{ fontSize: 20, fontWeight: 600 }}>Total sales</p>
+         
+
+            <div className='pro-scroll' style={{ height: 360, overflowY: 'scroll' }} >
+              <Table />
+            </div>
+          </Paper>
+        </Grid>
       </Grid>
-      <div style={{height:20}} />
-      <Grid xs={12}>
-        <Paper className={classes.paper}>
-          <p style={{fontSize:20,fontWeight:600}}>Total sales</p>
-        <div className='pro-scroll' style={{ height: '200px', overflowY: 'scroll' }} >
-            <Table />
-          </div>
-        </Paper>
-      </Grid>
+
     </div>
   );
 }
